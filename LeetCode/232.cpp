5061ba5ac232ef43144f23f2bd882460f3852014
Nuyoah <91939836+Harper-Ge@users.noreply.github.com>
2023-03-15 16:33:18 +0800

    232

---
+++ LeetCode/232.cpp
@@ -0,0 +1,33 @@
+class MyQueue {
+public:
+    stack<int> stIn;
+    stack<int> stOut;
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        stIn.push(x);
+    }
+    
+    int pop() {
+        if(stOut.empty())
+            while(!stIn.empty()){
+                stOut.push(stIn.top());
+                stIn.pop();
+            }
+        int res = stOut.top();
+        stOut.pop();
+        return res;
+    }
+    
+    int peek() {
+        int res = this->pop();
+        stOut.push(res);
+        return res;
+    }
+    
+    bool empty() {
+        return stIn.empty() && stOut.empty();
+    }
+};
